96b7ccd4b40ca8ee85890627a83d25f006abd86a
Mercy294 <mercyniblett11@gmail.com>
2025-11-13 18:33:25 +0300

    modified the borrow_book file

--- borrow_book/borrow_book.py
+++ borrow_book/borrow_book.py
@@ -3,6 +3,6 @@ def borrow_book(book_title, available_books):
         available_books.remove(book_title)
         return f"You have borrowed '{book_title}'."
     else:
-        return f"Sorry, '{book_title}' is not available."
+        return f"Sorry, this '{book_title}' is not available."
 
 
