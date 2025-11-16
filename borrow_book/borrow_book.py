a18843f8ddc7aadcb8ece5617dae4bdf8a2d5cf3
Mercy294 <mercyniblett11@gmail.com>
2025-11-16 15:50:35 +0300

    modifed borrow_book file

--- borrow_book/borrow_book.py
+++ borrow_book/borrow_book.py
@@ -1,8 +1,8 @@
-# def borrow_book(book_title, available_books):
-#     if book_title in available_books:
-#         available_books.remove(book_title)
-#         return f"You have borrowed '{book_title}'."
-#     else:
-#         return f"Sorry, this '{book_title}' is not available."
+def borrow_book(book_title, available_books):
+    if book_title in available_books:
+        available_books.remove(book_title)
+        return f"You have borrowed '{book_title}'."
+    else:
+        return f"Sorry, this '{book_title}' is not available."
 
 
